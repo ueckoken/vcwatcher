67b9fb5e23da893aaf6adbe844d5dd9a32cf38a4
Laddge <dev.laddge@gmail.com>
2026-02-02 08:23:48 +0900

    add voice state

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -3,9 +3,14 @@ import { onMount } from 'svelte'
 import { format } from 'date-fns'
 
 type Member = {
-  userId: string
+  id: string
+  username: string
   displayName: string
   displayAvatarURL: string
+  deaf: boolean
+  mute: boolean
+  video: boolean
+  streaming: boolean
 }
 
 let now: Date
@@ -22,19 +27,34 @@ onMount(() => {
 </script>
 
 <main>
-  <div class="px-3 py-2 font-mono font-bold flex justify-between bg-base-200 sticky top-0">
+  <div class="p-6 text-2xl font-mono font-bold flex items-center justify-between bg-base-200 sticky top-0">
     <div>{now && format(now, 'HH:mm')}</div>
     <div class="flex items-center gap-2">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user h-4 w-4"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
       {members.length}
     </div>
   </div>
-  <div class="p-3 flex flex-col gap-4">
+  <div class="p-8 flex flex-col gap-6">
     {#each members as member}
-      <div class="flex items-center gap-3">
-        <img src={member.displayAvatarURL} alt={member.userId} class="h-8 rounded-full" />
+      <div class="flex items-center gap-6">
+        <img src={member.displayAvatarURL} alt={member.id} class="h-14 rounded-full" />
         <div class="break-all">
-          {member.displayName}
+          <div class="text-2xl">{member.displayName}</div>
+          <div class="text-sm">@{member.username}</div>
+        </div>
+        <div class="grow flex justify-end gap-4">
+          {#if member.deaf}
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-headphone-off-icon lucide-headphone-off"><path d="M21 14h-1.343"/><path d="M9.128 3.47A9 9 0 0 1 21 12v3.343"/><path d="m2 2 20 20"/><path d="M20.414 20.414A2 2 0 0 1 19 21h-1a2 2 0 0 1-2-2v-3"/><path d="M3 14h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-7a9 9 0 0 1 2.636-6.364"/></svg>
+          {/if}
+          {#if member.mute}
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mic-off-icon lucide-mic-off"><path d="M12 19v3"/><path d="M15 9.34V5a3 3 0 0 0-5.68-1.33"/><path d="M16.95 16.95A7 7 0 0 1 5 12v-2"/><path d="M18.89 13.23A7 7 0 0 0 19 12v-2"/><path d="m2 2 20 20"/><path d="M9 9v3a3 3 0 0 0 5.12 2.12"/></svg>
+          {/if}
+          {#if member.video}
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-video-icon lucide-video"><path d="m16 13 5.223 3.482a.5.5 0 0 0 .777-.416V7.87a.5.5 0 0 0-.752-.432L16 10.5"/><rect x="2" y="6" width="14" height="12" rx="2"/></svg>
+          {/if}
+          {#if member.streaming}
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-monitor-icon lucide-monitor"><rect width="20" height="14" x="2" y="3" rx="2"/><line x1="8" x2="16" y1="21" y2="21"/><line x1="12" x2="12" y1="17" y2="21"/></svg>
+          {/if}
         </div>
       </div>
     {/each}
--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,17 @@ import { type WSContext } from 'hono/ws'
 import { Client, Events, GatewayIntentBits, type VoiceChannel } from 'discord.js'
 import 'dotenv/config'
 
+type Member = {
+  id: string
+  username: string
+  displayName: string
+  displayAvatarURL: string
+  deaf: boolean|null
+  mute: boolean|null
+  video: boolean|null
+  streaming: boolean|null
+}
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -17,6 +28,19 @@ const client = new Client({
 let vcChannel: VoiceChannel|undefined
 const wsClients: WSContext[] = []
 
+function getMembers(): Member[] {
+  return vcChannel?.members.map(member => ({
+    id: member.user.id,
+    username: member.user.username,
+    displayName: member.displayName,
+    displayAvatarURL: member.displayAvatarURL(),
+    deaf: member.voice.deaf,
+    mute: member.voice.mute,
+    video: member.voice.selfVideo,
+    streaming: member.voice.streaming,
+  })) || []
+}
+
 client.on(Events.ClientReady, readyClient => {
   console.log(`Logged in as ${readyClient.user.tag}!`)
   vcChannel = client.channels.cache.get(process.env.VC_CHANNEL || '') as VoiceChannel|undefined
@@ -26,9 +50,9 @@ client.on(Events.ClientReady, readyClient => {
 client.on(Events.VoiceStateUpdate, (oldState, newState) => {
   if (!vcChannel) return
   if (newState.channel?.id === vcChannel.id || oldState.channel?.id === vcChannel.id) {
-    newState.member && console.log(newState.member.user.username, newState.channel ? 'connected.' : 'disconnected.')
+    newState.member && oldState.channel?.id !== newState.channel?.id && console.log(newState.member.user.username, newState.channel ? 'connected.' : 'disconnected.')
     for (const ws of wsClients) {
-      ws.send(JSON.stringify({ members: vcChannel?.members }))
+      ws.send(JSON.stringify({ members: getMembers() }))
     }
   }
 })
@@ -42,7 +66,7 @@ app.use("*", serveStatic({ root: './frontend/dist' }))
 app.get('/ws', upgradeWebSocket((_c) => ({
   onOpen(_event, ws) {
     wsClients.push(ws)
-    ws.send(JSON.stringify({ members: vcChannel?.members }))
+    ws.send(JSON.stringify({ members: getMembers() }))
   },
   onClose(_event, ws) {
     const i = wsClients.indexOf(ws)
